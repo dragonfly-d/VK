29de76d07d3809dff123dd0914fc672fe943c37b
Dmitry <dragonfly_d@mail.ru>
2014-08-14 00:16:20 +0400

    private members added

--- VK_api.py
+++ VK_api.py
@@ -8,12 +8,12 @@ class VK:
     This class provides access to VK API
     """
     def __init__(self, user_id, token):
-        self.user_id = user_id
-        self.token = token
-        self.url = u'https://api.vk.com/method/%s'
+        self.__user_id = user_id
+        self.__token = token
+        self.__url = u'https://api.vk.com/method/%s'
 
     def __get_data(self, method, params):
-        res = urllib2.urlopen(self.url % method, params).read()
+        res = urllib2.urlopen(self.__url % method, params).read()
         data = json.loads(res)
         if 'error' in data:
             print data
@@ -21,13 +21,13 @@ class VK:
         return data[u'response']
 
     def get_friends(self):
-        return self.__get_data('friends.get', 'user_id=%s&fields=nickname' % self.user_id)
+        return self.__get_data('friends.get', 'user_id=%s&fields=nickname' % self.__user_id)
 
     def get_audios(self):
-        return self.__get_data('audio.get', 'owner_id=%s&access_token=%s' % (self.user_id, self.token))
+        return self.__get_data('audio.get', 'owner_id=%s&access_token=%s' % (self.__user_id, self.__token))
 
     def get_groups(self):
-        return self.__get_data('groups.get', 'user_id=%s&access_token=%s' % (self.user_id, self.token))
+        return self.__get_data('groups.get', 'user_id=%s&access_token=%s' % (self.__user_id, self.__token))
 
     def get_users_info(self, ids):
         return self.__get_data('users.get', 'user_ids=%s' % ids)
@@ -36,4 +36,4 @@ class VK:
         return self.__get_data('groups.getById', 'group_ids=%s&fields=contacts,description,members_count' % groups_id)
 
     def get_account_permissions(self):
-        return self.__get_data('account.getAppPermissions', 'user_ids=%s&access_token=' + self.token)
+        return self.__get_data('account.getAppPermissions', 'user_ids=%s&access_token=' + self.__token)
